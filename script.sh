c17c2b03b0884d4d74995329c9d1348f39543e83
AlexanderIlin2005 <sashailyin2005@gmail.com>
2024-11-08 11:13:00 +0300

    -

--- script.sh
+++ script.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Запускаем первую команду в новом окне
-osascript -e 'tell application "Terminal" to do script "ssh -p 2222 s381032@se.ifmo.ru -Y -L3565:helios:3565"'
+osascript -e 'tell application "Terminal" to do script "ssh -p 2222 s381032@se.ifmo.ru -Y -L24082:helios:24082"'
 
 # Пауза для уверенности, что первая команда стартовала
 sleep 2
 
 # Запускаем вторую команду в другом новом окне
-osascript -e 'tell application "Terminal" to do script "ssh -p 2222 s381032@se.ifmo.ru -Y -L35650:helios:35650"'
+osascript -e 'tell application "Terminal" to do script "ssh -p 2222 s381032@se.ifmo.ru -Y -L24080:helios:24080"'
